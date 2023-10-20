79d1e8f315bda815e515b5a4e2b3d95bcbc225b4
SigmaDanAI <89607551+SigmaDanAI@users.noreply.github.com>
2023-10-20 02:55:05 -0300

    feat: added user profile change password button

--- screens/Dashboard.tsx
+++ screens/Dashboard.tsx
@@ -1,200 +1,220 @@
 import {
-    Dimensions,
-    ImageBackground,
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
-  } from "react-native";
-  import React from "react";
-  import Spacing from "../constants/Spacing";
-  import FontSize from "../constants/FontSize";
-  import Colors from "../constants/Colors";
-  import Font from "../constants/Font";
-  import { NativeStackScreenProps } from "@react-navigation/native-stack";
-  import { RootStackParamList } from "../types";
-  const { height } = Dimensions.get("window");
-  import { useUserStore } from '../stores/useUserStore';
+  Dimensions,
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import Spacing from "../constants/Spacing";
+import FontSize from "../constants/FontSize";
+import Colors from "../constants/Colors";
+import Font from "../constants/Font";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types";
+const { height } = Dimensions.get("window");
+import { useUserStore } from "../stores/useUserStore";
+
+type Props = NativeStackScreenProps<RootStackParamList, "Dashboard">;
+
+
+const Dashboard: React.FC<Props> = ({ navigation: { navigate } }) => {
+  const { userName, setUserName } = useUserStore();
+  const { userLastName, setUserLastName } = useUserStore();
+  const { userEmail, setUserEmail } = useUserStore();
+  const { removeAccessToken } = useUserStore();
+
+  const logout = async ()  => {
+    await removeAccessToken()
+    setUserName('')
+    setUserLastName('')
+    setUserEmail('')
+    navigate("Welcome")
+  }
   
-  type Props = NativeStackScreenProps<RootStackParamList, "Dashboard">;
-  
-  const Dashboard: React.FC<Props> = ({ navigation: { navigate } }) => {
-    return (
-      <SafeAreaView>
-        <View>
-          
-          <View
+  return (
+    <SafeAreaView>
+      <View>
+        <View
+          style={{
+            paddingHorizontal: Spacing * 4,
+            paddingTop: Spacing * 4,
+          }}
+        >
+          <Text
             style={{
-              paddingHorizontal: Spacing * 4,
-              paddingTop: Spacing * 4,
+              fontSize: FontSize.xxLarge,
+              color: Colors.primary,
+              fontFamily: Font["poppins-bold"],
+              textAlign: "center",
+            }}
+          >
+            Bienvenido 
+          </Text>
+          <Text
+          style={{
+            fontSize: FontSize.large,
+            fontFamily: Font["poppins-bold"],
+            textAlign: "center",
+          }}>
+            {userName}
+          </Text>
+        </View>
+      </View>
+      <View
+          style={{
+            paddingHorizontal: Spacing * 2,
+            paddingTop: Spacing * 6,
+            flexDirection: "row",
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => navigate("UserProfile")}
+            style={{
+              marginLeft: 90,
+              backgroundColor: Colors.primary,
+              paddingVertical: Spacing * 1.5,
+              paddingHorizontal: Spacing * 2,
+              width: "48%",
+              borderRadius: Spacing,
+              shadowColor: Colors.primary,
+              shadowOffset: {
+                width: 0,
+                height: Spacing,
+              },
+              shadowOpacity: 0.3,
+              shadowRadius: Spacing,
             }}
           >
             <Text
               style={{
-                fontSize: FontSize.xxLarge,
-                color: Colors.primary,
                 fontFamily: Font["poppins-bold"],
+                color: Colors.onPrimary,
+                fontSize: FontSize.large,
                 textAlign: "center",
               }}
             >
-              Bienvenido
+              Tu Perfil
             </Text>
-          </View>
-          
-          <View
+          </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          paddingHorizontal: Spacing * 2,
+          paddingTop: Spacing * 6,
+          flexDirection: "row",
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => navigate("Dashboard")}
+          style={{
+            marginLeft: 90,
+            backgroundColor: Colors.primary,
+            paddingVertical: Spacing * 1.5,
+            paddingHorizontal: Spacing * 2,
+            width: "48%",
+            borderRadius: Spacing,
+            shadowColor: Colors.primary,
+            shadowOffset: {
+              width: 0,
+              height: Spacing,
+            },
+            shadowOpacity: 0.3,
+            shadowRadius: Spacing,
+          }}
+        >
+          <Text
             style={{
-              paddingHorizontal: Spacing * 2,
-              paddingTop: Spacing * 6,
-              flexDirection: "row",
+              fontFamily: Font["poppins-bold"],
+              color: Colors.onPrimary,
+              fontSize: FontSize.large,
+              textAlign: "center",
             }}
           >
-            <TouchableOpacity
-              onPress={() => navigate("UserProfile")}
-              style={{
-                marginLeft: 90,
-                backgroundColor: Colors.primary,
-                paddingVertical: Spacing * 1.5,
-                paddingHorizontal: Spacing * 2,
-                width: "48%",
-                borderRadius: Spacing,
-                shadowColor: Colors.primary,
-                shadowOffset: {
-                  width: 0,
-                  height: Spacing,
-                },
-                shadowOpacity: 0.3,
-                shadowRadius: Spacing,
-              }}
-            >
-              <Text
-                style={{
-                  fontFamily: Font["poppins-bold"],
-                  color: Colors.onPrimary,
-                  fontSize: FontSize.large,
-                  textAlign: "center",
-                }}
-              >
-                User Profile
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <View
+            Ver Equipos
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          paddingHorizontal: Spacing * 2,
+          paddingTop: Spacing * 6,
+          flexDirection: "row",
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => navigate("TeamCreationScreen")}
+          style={{
+            marginLeft: 90,
+            backgroundColor: Colors.primary,
+            paddingVertical: Spacing * 1.5,
+            paddingHorizontal: Spacing * 2,
+            width: "48%",
+            borderRadius: Spacing,
+            shadowColor: Colors.primary,
+            shadowOffset: {
+              width: 0,
+              height: Spacing,
+            },
+            shadowOpacity: 0.3,
+            shadowRadius: Spacing,
+          }}
+        >
+          <Text
             style={{
-              paddingHorizontal: Spacing * 2,
-              paddingTop: Spacing * 6,
-              flexDirection: "row",
+              fontFamily: Font["poppins-bold"],
+              color: Colors.onPrimary,
+              fontSize: FontSize.large,
+              textAlign: "center",
             }}
           >
-            <TouchableOpacity
-              onPress={() => navigate("Login")}
-              style={{
-                marginLeft: 90,
-                backgroundColor: Colors.primary,
-                paddingVertical: Spacing * 1.5,
-                paddingHorizontal: Spacing * 2,
-                width: "48%",
-                borderRadius: Spacing,
-                shadowColor: Colors.primary,
-                shadowOffset: {
-                  width: 0,
-                  height: Spacing,
-                },
-                shadowOpacity: 0.3,
-                shadowRadius: Spacing,
-              }}
-            >
-              <Text
-                style={{
-                  fontFamily: Font["poppins-bold"],
-                  color: Colors.onPrimary,
-                  fontSize: FontSize.large,
-                  textAlign: "center",
-                }}
-              >
-                Ver equipos
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <View
+            Crear Equipo
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          paddingHorizontal: Spacing * 2,
+          paddingTop: Spacing * 6,
+          flexDirection: "row",
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => logout()}
+          style={{
+            marginLeft: 90,
+            backgroundColor: Colors.primary,
+            paddingVertical: Spacing * 1.5,
+            paddingHorizontal: Spacing * 2,
+            width: "48%",
+            borderRadius: Spacing,
+            shadowColor: Colors.primary,
+            shadowOffset: {
+              width: 0,
+              height: Spacing,
+            },
+            shadowOpacity: 0.3,
+            shadowRadius: Spacing,
+          }}
+        >
+          <Text
             style={{
-              paddingHorizontal: Spacing * 2,
-              paddingTop: Spacing * 6,
-              flexDirection: "row",
+              fontFamily: Font["poppins-bold"],
+              color: Colors.onPrimary,
+              fontSize: FontSize.large,
+              textAlign: "center",
             }}
           >
-            <TouchableOpacity
-              onPress={() => navigate("TeamCreationScreen")}
-              style={{
-                marginLeft: 90,
-                backgroundColor: Colors.primary,
-                paddingVertical: Spacing * 1.5,
-                paddingHorizontal: Spacing * 2,
-                width: "48%",
-                borderRadius: Spacing,
-                shadowColor: Colors.primary,
-                shadowOffset: {
-                  width: 0,
-                  height: Spacing,
-                },
-                shadowOpacity: 0.3,
-                shadowRadius: Spacing,
-              }}
-            >
-              <Text
-                style={{
-                  fontFamily: Font["poppins-bold"],
-                  color: Colors.onPrimary,
-                  fontSize: FontSize.large,
-                  textAlign: "center",
-                }}
-              >
-                Crear Equipo
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <View
-            style={{
-              paddingHorizontal: Spacing * 2,
-              paddingTop: Spacing * 6,
-              flexDirection: "row",
-            }}
-          >
-            <TouchableOpacity
-              onPress={() => navigate("Login")}
-              style={{
-                marginLeft: 90,
-                backgroundColor: Colors.primary,
-                paddingVertical: Spacing * 1.5,
-                paddingHorizontal: Spacing * 2,
-                width: "48%",
-                borderRadius: Spacing,
-                shadowColor: Colors.primary,
-                shadowOffset: {
-                  width: 0,
-                  height: Spacing,
-                },
-                shadowOpacity: 0.3,
-                shadowRadius: Spacing,
-              }}
-            >
-              <Text
-                style={{
-                  fontFamily: Font["poppins-bold"],
-                  color: Colors.onPrimary,
-                  fontSize: FontSize.large,
-                  textAlign: "center",
-                }}
-              >
-                Salir
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </SafeAreaView>
-    );
-  };
-  
-  export default Dashboard;
-  
-  const styles = StyleSheet.create({});
+            Cerrar Sesión
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Dashboard;
+
+const styles = StyleSheet.create({});
